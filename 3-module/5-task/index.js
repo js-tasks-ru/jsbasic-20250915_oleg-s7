8b1bd737314c42f26d3a404c9cfd5d6ee3502bdc
OlegFleet <solyakov1@yandex.ru>
2025-09-30 17:29:24 +0300

    proshu proverit 3.5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let parts = str.split(' ');
+  let numbers = parts
+    .map(item => parseFloat(item))
+    .filter(num => !isNaN(num));
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
