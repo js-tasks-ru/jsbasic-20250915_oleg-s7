0d5ac33b7020fb3f393dc08a68efea2a6809a680
OlegFleet <solyakov1@yandex.ru>
2025-10-06 17:04:28 +0300

    proshu proverit 4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.tBodies[0].rows) {
+    let ageCell = row.cells[1];
+    let genderCell = row.cells[2];
+    let statusCell = row.cells[3];
+
+    if (statusCell.hasAttribute('data-available')) {
+      if (statusCell.dataset.available === "true") {
+        row.classList.add('available');
+      } else if (statusCell.dataset.available === "false") {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.hidden = true;
+    }
+
+    if (genderCell.textContent === 'm') {
+      row.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      row.classList.add('female');
+    }
+
+    if (Number(ageCell.textContent) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
